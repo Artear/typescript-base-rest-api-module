ade5adaf71af6eb8308b37af7e61085064ac329e
Gabriel Fusca <gfusca@artear.com>
2017-06-22 14:03:19 -0300

    changed revert abstract method declarations

--- src/helper/logger/Logger.ts
+++ src/helper/logger/Logger.ts
@@ -1,9 +1,9 @@
 export abstract class Logger {
     protected level: string;
-    public abstract info(msg: string | Object): void;
-    public abstract debug(msg: string | Object): void;
-    public abstract warn(msg: string | Object): void;
-    public abstract error(msg: string | Object | Error): void;
+    public info(msg: string | Object): void {};
+    public debug(msg: string | Object): void{};
+    public warn(msg: string | Object): void {};
+    public error(msg: string | Object | Error): void {};
 
     public setLevel(level: string): void {
         this.level = level;
