80cbf9f0687ec18a57c3a71944b438131c775da1
Gabriel Fusca <gfusca@artear.com>
2017-06-22 14:02:29 -0300

    change build folder update package.json add declaration files (d.ts)

---
+++ index.ts
@@ -0,0 +1,43 @@
+export * from "./src/helper/validation/restify/validation";
+
+export {Logger} from "./src/helper/logger/Logger";
+
+export * from "./src/helper/logger/LogBuilder";
+
+export * from "./src/helper/logger/DevelopLogConfig";
+
+export {LogConfig} from "./src/helper/logger/LogConfig";
+
+export * from "./src/helper/logger/LoggerHelper";
+
+export * from "./src/helper/logger/LogHandler";
+
+export * from "./src/helper/logger/NewrelicLogger";
+
+export * from "./src/helper/logger/ProductionLogConfig";
+
+export * from "./src/helper/logger/ServerLogger";
+
+export {DataSource} from "./src/data_source/base/DataSource";
+
+export {DataSourceManager} from "./src/data_source/base/DataSourceManager";
+
+export {DynamoDataSource} from "./src/data_source/DynamoDataSource";
+
+export {Connection} from "./src/data_source/dynamo/Connection";
+
+export {MemoryDataSource} from "./src/data_source/MemoryDataSource";
+
+export {ExternalDataSource} from "./src/data_source/ExternalDataSource";
+
+export {RouterConfig} from "./src/router/BaseRouter";
+
+export {PingRouter} from "./src/router/PingRouter";
+
+export {BaseService} from "./src/services/BaseService";
+
+export {ServerBuilder} from "./src/server/ServerBuilder";
+
+export {OptionsBuilder} from "./src/server/OptionsBuilder";
+
+
