55a16bdc84f0ce059b65f4584d49db241ea0666b
Gabriel Fusca <gfusca@artear.com>
2017-06-27 14:29:52 -0300

    Changed: verify if has dynamodb config settings before build aws dynamo config

--- src/data_source/dynamo/Connection.ts
+++ src/data_source/dynamo/Connection.ts
@@ -3,16 +3,18 @@ import {DynamoDB} from "aws-sdk";
 import * as config from "config";
 import DocumentClient = DynamoDB.DocumentClient;
 
-AWS.config.dynamodb = {
-    region: config.get<string>("aws.region"),
-    endpoint: config.get<string>("aws.dynamodb.endpoint"),
-    accessKeyId: config.get<string>("aws.accessKeyId"),
-    secretAccessKey: config.get<string>("aws.secretAccessKey"),
-    maxRetries: config.get<number>("aws.dynamodb.maxRetries")
-};
+if (config.has("aws.dynamodb")) {
+    AWS.config.dynamodb = {
+        region: config.get<string>("aws.region"),
+        endpoint: config.get<string>("aws.dynamodb.endpoint"),
+        accessKeyId: config.get<string>("aws.accessKeyId"),
+        secretAccessKey: config.get<string>("aws.secretAccessKey"),
+        maxRetries: config.get<number>("aws.dynamodb.maxRetries")
+    };
 
-AWS.config.httpOptions.timeout = parseInt(config.get<string>("aws.dynamodb.timeout"));
-AWS.config.maxRetries = parseInt(config.get<string>("aws.dynamodb.maxRetries"));
+    AWS.config.httpOptions.timeout = parseInt(config.get<string>("aws.dynamodb.timeout"));
+    AWS.config.maxRetries = parseInt(config.get<string>("aws.dynamodb.maxRetries"));
+}
 
 export class Connection {
 
