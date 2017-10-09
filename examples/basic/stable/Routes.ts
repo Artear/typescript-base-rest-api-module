8c1efa115833e6bdc2c640b8bd91e458f24aa3d7
leanazulyoro <leanazulyoro@gmail.com>
2017-10-09 11:51:04 -0300

    Changed: Added ping router to both stable and current versions

--- examples/basic/stable/Routes.ts
+++ examples/basic/stable/Routes.ts
@@ -1,9 +1,11 @@
 import {ItemRouter} from "./ItemRouter";
 import * as config from "config";
 import {RouterListBuilder} from "../../../src/router/RouterListBuilder";
+import {PingRouter} from "../../../src/router/PingRouter";
 
 const RouterBuilder = (new RouterListBuilder(config.get<string>("server.options.apiVersion.stable")))
-                .withRouter(ItemRouter)
-                .build();
+    .withRouter(PingRouter)
+    .withRouter(ItemRouter)
+    .build();
 
 export default RouterBuilder;
