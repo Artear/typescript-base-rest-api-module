5b2ed03a9d8abbb5f668c35a034fdea5d423f500
Brian Novillo <brian_novillo@artear.com>
2017-10-30 14:53:07 -0300

    Add router and service for expose _version.txt

--- examples/basic/stable/Routes.ts
+++ examples/basic/stable/Routes.ts
@@ -2,10 +2,12 @@ import {ItemRouter} from "./ItemRouter";
 import * as config from "config";
 import {RouterListBuilder} from "../../../src/router/RouterListBuilder";
 import {PingRouter} from "../../../src/router/PingRouter";
+import {VersionRouter} from "../../../src/router/VersionRouter";
 
 const RouterBuilder = (new RouterListBuilder(config.get<string>("server.options.apiVersion.stable")))
     .withRouter(PingRouter)
     .withRouter(ItemRouter)
+    .withRouter(VersionRouter)
     .build();
 
 export default RouterBuilder;
