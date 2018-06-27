4ffcb782d92ce48931c3a423f36526dd1bb04afa
walterscarano <wscarano@artear.com>
2018-06-27 17:02:06 -0300

    GET requires authorization header

---
+++ src/server/SecurityMiddleware.ts
@@ -0,0 +1,13 @@
+import * as config from "config";
+import { NotAuthorizedError } from "restify-errors";
+
+export const securityMiddleware = (req, res, next) => {
+  if (shouldValidateMethod(req.method) && !tokenIsValid(req.headers["authorization"])) {
+      return res.send(new NotAuthorizedError("invalid Token"));
+  }
+  next();
+}
+
+export const shouldValidateMethod = method => ["GET", "POST", "PUT", "PATCH", "DELETE"].includes(method);
+
+export const tokenIsValid = token => !!token && config.get<string>("security.token") === token;
