b5786d2e01d557a65fe8d0de74af357b77ad20c0
walterscarano <wscarano@artear.com>
2018-07-16 15:53:44 -0300

    /ping excluded

--- src/server/SecurityMiddleware.ts
+++ src/server/SecurityMiddleware.ts
@@ -2,12 +2,12 @@ import * as config from "config";
 import { NotAuthorizedError } from "restify-errors";
 
 export const securityMiddleware = (req, res, next) => {
-  if (shouldValidateMethod(req.method) && !tokenIsValid(req.headers["authorization"])) {
+  if (shouldValidateMethod(req.method) && shouldValidatePath(req.route.path) && !tokenIsValid(req.headers["authorization"])) {
       return res.send(new NotAuthorizedError("invalid Token"));
   }
   next();
 }
 
 export const shouldValidateMethod = method => ["GET", "POST", "PUT", "PATCH", "DELETE"].indexOf(method) !== -1;
-
+export const shouldValidatePath = path => ['/ping'].indexOf(path) === -1;
 export const tokenIsValid = token => !!token && config.get<string>("security.token") === token;
