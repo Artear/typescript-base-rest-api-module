45f9d82c05a43a859c18846fd34689ca5d55bef0
Brian Novillo <brian@briannovillo.com.ar>
2018-11-21 12:47:07 -0300

    Improve Dynamodb queries with multiple ids using a single batch call (#38)
    * Improve Dynamodb queries with multiple ids using a single batch call
    
    * Update done() position for error on circleci:
    For async tests and hooks, ensure "done()" is called; if returning a Promise, ensure it resolves.
    
    * Update version for fix on tests
    
    * Tests fixed (#39)

--- test/dynamo-data-source-spec.ts
+++ test/dynamo-data-source-spec.ts
@@ -9,21 +9,56 @@ import {mockLoader} from "./mocks/mockHelper";
 import DocumentClient = DynamoDB.DocumentClient;
 
 
-describe("DynamoDataSource Test", function () {
+describe("DynamoDataSource fields test", function(){
 
     let documentClientStub;
+    let connectionStub;
+    let documentClient;
+    let dataSource: DynamoDataSource;
+    documentClient = new DocumentClient();
+
+    beforeEach( () => {
+        dataSource = new DynamoDataSource();
+        connectionStub = sinon.stub(Connection, "getInstance").callsFake(function () {
+            return documentClient;
+        });
+    })
+
+    afterEach(() => {
+        connectionStub.restore();
+    });
+
+    it("Should receive fields to filter on DynamoDataSource", (done: Function) => {
+        const key = "some_key";
+        const fields = "itemId";
+        documentClientStub = sinon.stub(documentClient, "get").callsFake(function (params: any, next: (err: any, data: any) => void) {
+            expect(params["ProjectionExpression"]).to.be.equal(fields);
+            done();
+        });
+
+        dataSource.getData(key, fields).then();
+    });
+});
+
+describe("DynamoDataSource CRUD Test", function () {
+
+    let documentClientStub;
+    let connectionStub;
     let documentPutClientStub;
     let dataSource: DynamoDataSource;
     let mockedBody;
     let documentClient;
-    const connectionStub = sinon.stub(Connection, "getInstance").callsFake(function () {
-        return documentClient;
-    });
 
     beforeEach(() => {
         mockedBody = mockLoader(itemMock);
         dataSource = new DynamoDataSource();
         documentClient = new DocumentClient();
+        const tableName = "Block";
+        dataSource['table'] = tableName;
+
+        connectionStub = sinon.stub(Connection, "getInstance").callsFake(function () {
+            return documentClient;
+        });
 
         documentClientStub = sinon.stub(documentClient, "get").callsFake(function (params, callback) {
             if (params.Key.itemId === mockedBody.itemId) {
@@ -33,29 +68,21 @@ describe("DynamoDataSource Test", function () {
             }
         });
 
+        documentClientStub = sinon.stub(documentClient, "batchGet").callsFake(function (params, callback) {
+            callback(null, {Responses: {[tableName]: [mockedBody]}});
+        });
+
         documentPutClientStub = sinon.stub(documentClient, "put").callsFake(function (params, callback) {
             callback(null, mockedBody);
         });
 
     });
 
-    after(() => {
+    afterEach(() => {
         documentClientStub.restore();
         documentPutClientStub.restore();
+        connectionStub.restore();
     });
-
-    it("Should receive fields to filter on DynamoDataSource", (done: Function) => {
-        const key = "some_key";
-        const fields = "itemId";
-        documentClientStub.restore();
-        documentClientStub = sinon.stub(documentClient, "get").callsFake(function (params: any, next: (err: any, data: any) => void) {
-            expect(params["ProjectionExpression"]).to.be.equal(fields);
-            done();
-        });
-
-        dataSource.getData(key, fields).then();
-    });
-
     it("Should get items by id", (done: Function) => {
         dataSource.getItems([mockedBody.itemId]).then(function (data) {
             chai.expect(data[0]).to.equal(mockedBody);
@@ -64,7 +91,6 @@ describe("DynamoDataSource Test", function () {
     });
 
     it("PutData Should return item id inserted", (done: Function) => {
-        dataSource = new DynamoDataSource();
         dataSource.putData(mockedBody.itemId, mockedBody).then(function (data) {
             chai.expect(data.itemId).to.equal(mockedBody.itemId);
             done();
@@ -72,7 +98,6 @@ describe("DynamoDataSource Test", function () {
     });
 
     it("Updated Should return item id updated", (done: Function) => {
-        dataSource = new DynamoDataSource();
         mockedBody.content.title.main = "dummy title";
         dataSource.updateData(mockedBody.itemId, mockedBody).then(function (data) {
             chai.expect(data.itemId).to.equal(mockedBody.itemId);
