520f3487fead533aa340a8de67b8cc4bb88e03bd
walterscarano <wscarano@artear.com>
2018-11-27 17:46:39 -0300

    Tests

--- src/server/SecurityMiddleware.ts
+++ src/server/SecurityMiddleware.ts
@@ -1,10 +1,9 @@
 import { NotAuthorizedError } from "restify-errors";
 
 export const withSecurity = token => (next) => (req, res) => {
-    if (shouldValidateMethod(req.method) && !tokenIsValid(token, req.headers["authorization"])) {
+    if (!tokenIsValid(token, req.headers["authorization"])) {
         return res.send(new NotAuthorizedError("invalid Token"));
     }
     next();
 };
-export const shouldValidateMethod = method => ["GET", "POST", "PUT", "PATCH", "DELETE"].indexOf(method) !== -1;
 export const tokenIsValid = (token, authorization) => !!authorization && authorization === token;
